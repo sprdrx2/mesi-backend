10147dc16031cc287cfc0303dd77c42bb7b907e9
tobie.tourre <tobie.tourre@ipilyon.net>
2019-07-16 10:30:25 +0200

    create entity Venue

---
+++ src/Entity/Venue.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VenueRepository")
+ */
+class Venue
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $yelp_id;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $espace_poussette;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $table_langer;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $table_langer_men;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $menu_enfant;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $espace_jeu;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getYelpId(): ?string
+    {
+        return $this->yelp_id;
+    }
+
+    public function setYelpId(string $yelp_id): self
+    {
+        $this->yelp_id = $yelp_id;
+
+        return $this;
+    }
+
+    public function getEspacePoussette(): ?bool
+    {
+        return $this->espace_poussette;
+    }
+
+    public function setEspacePoussette(?bool $espace_poussette): self
+    {
+        $this->espace_poussette = $espace_poussette;
+
+        return $this;
+    }
+
+    public function getTableLanger(): ?bool
+    {
+        return $this->table_langer;
+    }
+
+    public function setTableLanger(?bool $table_langer): self
+    {
+        $this->table_langer = $table_langer;
+
+        return $this;
+    }
+
+    public function getTableLangerMen(): ?bool
+    {
+        return $this->table_langer_men;
+    }
+
+    public function setTableLangerMen(?bool $table_langer_men): self
+    {
+        $this->table_langer_men = $table_langer_men;
+
+        return $this;
+    }
+
+    public function getMenuEnfant(): ?bool
+    {
+        return $this->menu_enfant;
+    }
+
+    public function setMenuEnfant(?bool $menu_enfant): self
+    {
+        $this->menu_enfant = $menu_enfant;
+
+        return $this;
+    }
+
+    public function getEspaceJeu(): ?bool
+    {
+        return $this->espace_jeu;
+    }
+
+    public function setEspaceJeu(?bool $espace_jeu): self
+    {
+        $this->espace_jeu = $espace_jeu;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20190716082750.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190716082750 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE venue (id INT AUTO_INCREMENT NOT NULL, yelp_id VARCHAR(255) NOT NULL, espace_poussette TINYINT(1) DEFAULT NULL, table_langer TINYINT(1) DEFAULT NULL, table_langer_men TINYINT(1) DEFAULT NULL, menu_enfant TINYINT(1) DEFAULT NULL, espace_jeu TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE venue');
+    }
+}
