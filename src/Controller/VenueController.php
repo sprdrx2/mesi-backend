6def9a720435e50d8f46c5180ee4214eb4b41aee
tobie.tourre <tobie.tourre@ipilyon.net>
2019-07-16 16:08:41 +0200

    add deleteAction()

--- src/Controller/VenueController.php
+++ src/Controller/VenueController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Venue;
+use phpDocumentor\Reflection\Types\Void_;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -91,5 +92,29 @@ class VenueController extends AbstractController
         $entityManager->flush($venue);
 
         return $this->json($venue);
+    }
+
+    /**
+     * @Route("/venue/{yelp_id}", name="delete", methods={"DELETE"})
+     */
+    public function deleteAction(Request $request, $yelp_id)
+    {
+        $venue = $this->getDoctrine()
+            ->getRepository(Venue::class)
+            ->findOneBy(["yelp_id" => $yelp_id]);
+
+        if(is_null($venue)){
+            throw new HttpException(400, "Venue doesn't exist!");
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($venue);
+        $entityManager->flush();
+
+        $venues = $this->getDoctrine()
+            ->getRepository(Venue::class)
+            ->findAll();
+
+        return $this->json($venues);
     }
 }
